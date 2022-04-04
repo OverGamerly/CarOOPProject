d7946a7ad6015b499b1d2594c07a38a2ffc253b7
OverGamerly <Kirill.Drozd15@gmail.com>
2022-04-04 17:54:38 +0300

    Add small project changes

--- CarsClass/Car.h
+++ CarsClass/Car.h
@@ -1,6 +1,5 @@
 #pragma once
-#include <iostream>
-#include <string>
+#include "MainHead.h"
 
 using namespace std;
 
--- CarsClass/Main.cpp
+++ CarsClass/Main.cpp
@@ -1,16 +1,8 @@
 #include "MainHead.h"
-#include "Cars.h"
+#include "Car.h"
 
 int main()
 {
-	Cars cr;
-
-	cr.setBrand("Audi");
-	cr.setModel("100 C3");
-	cr.setAge(1983);
-	cr.setCost(2500);
-	
-	cout << cr.getInfo() << endl;
 
 	return 0;
 }
