a09ab97258eede5dfb35c8db87ffcb65eab4b449
OverGamerly <Kirill.Drozd15@gmail.com>
2022-04-11 18:44:04 +0300

    Added manager for car park

---
+++ CarsClass/Manager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "CarPark.h"
+class Manager
+{
+
+public:
+	void sortByNameAcs(CarPark carpark);
+	void sortByNameDesc(CarPark carpark);
+	void sortByCostAcs(CarPark carpark);
+	void sortByCostDesc(CarPark carpark);
+	void sortByAgeAcs(CarPark carpark);
+	void sortByAgeDesc(CarPark carpark);
+
+	double calculateAvgCost(CarPark carpark);
+	double calculateAvgAge(CarPark carpark);
+
+	bool checkCar(CarPark carpark, Car car);
+};
+
